999cd08b563b8f6b048b44a1ca57838111a6f2f2
Peter <44701305+EyedNet@users.noreply.github.com>
2018-11-05 22:47:52 +0100

    Update rpt.js

--- assets/js/rpt.js
+++ assets/js/rpt.js
@@ -5,7 +5,7 @@ var inrecaptch=grecaptcha.getResponse();
 
 function start()
 {
- document.getElementById('submit').addEventListener('click',Checkrecaptcha,false);
+ d.getElementById('submit').addEventListener('click',Checkrecaptcha,false);
 }
 
       
@@ -15,15 +15,27 @@ function start()
    function Checkrecaptcha (evento)
         {
          var inrecaptch=grecaptcha.getResponse();
-         if(inrecaptch=="")
+	 var name=d.getElementById('name').value;
+         var email=d.getElementById('email').value;
+         var message=d.getElementById('massage').value;
+         if(name!=""||email!=""||message!="")
+	 {
+		  if(inrecaptch=="")
                 {
           alert("El captcha de google no ha sido activado...");
                  evento.preventDefault();		
 		               return false;
                 
                 }
-          else {alert ("El captcha ha sido activado.... ");
-               evento.preventDefault();		
-	             	return false;
+          else 
+	       {
+		       alert ("El captcha ha sido activado.... ");
+                       evento.preventDefault();		
+	               return false;
                }
+		 
+	 }
+		else {alert("Los campos del formulario deben ser rellenados correctamente...");}
+	 	
+        
         }
